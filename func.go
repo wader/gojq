9533a25e924a8acbf2e116eeda17bf5bb0539cb4
Mattias Wadman <mattias.wadman@gmail.com>
2021-02-01 19:38:24 +0100

    Add scope function to get all functions and varialbes in scope

--- func.go
+++ func.go
@@ -45,6 +45,7 @@ func init() {
 		"path":           argFunc1(nil),
 		"env":            argFunc0(nil),
 		"builtins":       argFunc0(nil),
+		"scope":          argFunc0(nil),
 		"input":          argFunc0(nil),
 		"modulemeta":     argFunc0(nil),
 		"length":         argFuncV0(funcLength),
