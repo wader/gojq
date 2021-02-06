ce5c30d22d0b773d83f570be6792ab3e290d0d8a
Mattias Wadman <mattias.wadman@gmail.com>
2021-02-06 16:26:09 +0100

    WIP: add scopedump function

--- func.go
+++ func.go
@@ -59,6 +59,7 @@ func init() {
 		"env":            argFunc0(nil),
 		"builtins":       argFunc0(nil),
 		"scope":          argFunc0(nil),
+		"scopedump":      argFunc0(nil),
 		"input":          argFunc0(nil),
 		"modulemeta":     argFunc0(nil),
 		"abs":            argFunc0(funcAbs),
