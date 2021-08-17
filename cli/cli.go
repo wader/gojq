9647ee83af3812cf7af4604fc6ce878873a0e97f
Mattias Wadman <mattias.wadman@gmail.com>
2021-08-17 11:20:43 +0200

    Rename fq gojq fork to github.com/wader/gojq

--- cli/cli.go
+++ cli/cli.go
@@ -11,7 +11,7 @@ import (
 
 	"github.com/mattn/go-isatty"
 
-	"github.com/itchyny/gojq"
+	"github.com/wader/gojq"
 )
 
 const name = "gojq"
--- cli/error.go
+++ cli/error.go
@@ -11,7 +11,7 @@ import (
 
 	"github.com/mattn/go-runewidth"
 
-	"github.com/itchyny/gojq"
+	"github.com/wader/gojq"
 )
 
 type emptyError struct {
