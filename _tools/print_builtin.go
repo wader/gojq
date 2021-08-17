31c759e54dd425d2492ec0a307bbb6f471d5a42d
Mattias Wadman <mattias.wadman@gmail.com>
2021-08-17 11:20:43 +0200

    Rename lexpos gojq fork to github.com/wader/gojq

--- _tools/print_builtin.go
+++ _tools/print_builtin.go
@@ -8,7 +8,7 @@ import (
 	"sort"
 	"strings"
 
-	"github.com/itchyny/gojq"
+	"github.com/wader/gojq"
 )
 
 func main() {
--- compiler_test.go
+++ compiler_test.go
@@ -11,7 +11,7 @@ import (
 	"time"
 	"unsafe"
 
-	"github.com/itchyny/gojq"
+	"github.com/wader/gojq"
 )
 
 func ExampleCompile() {
