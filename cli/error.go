f58dfa7ccd8d0749931df0b902457c05c8bfdff6
Mattias Wadman <mattias.wadman@gmail.com>
2021-08-17 11:20:43 +0200

    Rename fq gojq fork to github.com/wader/gojq

--- cli/error.go
+++ cli/error.go
@@ -12,7 +12,7 @@ import (
 	"github.com/itchyny/go-yaml"
 	"github.com/mattn/go-runewidth"
 
-	"github.com/itchyny/gojq"
+	"github.com/wader/gojq"
 )
 
 type emptyError struct {
--- option_environ_loader_test.go
+++ option_environ_loader_test.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/itchyny/gojq"
+	"github.com/wader/gojq"
 )
 
 func ExampleWithEnvironLoader() {
