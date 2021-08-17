50f44cf1246b23b0b57887863fa8bcd76ee0c0e3
Mattias Wadman <mattias.wadman@gmail.com>
2021-08-17 11:20:43 +0200

    Rename fq gojq fork to github.com/wader/gojq

--- _tools/print_builtin.go
+++ _tools/print_builtin.go
@@ -7,7 +7,7 @@ import (
 	"slices"
 	"strings"
 
-	"github.com/itchyny/gojq"
+	"github.com/wader/gojq"
 )
 
 func main() {
--- cli/inputs.go
+++ cli/inputs.go
@@ -10,7 +10,7 @@ import (
 
 	"github.com/itchyny/go-yaml"
 
-	"github.com/itchyny/gojq"
+	"github.com/wader/gojq"
 )
 
 type inputReader struct {
