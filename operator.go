036f968ed68a40130d275fcfcf2ab2217041ece0
Mattias Wadman <mattias.wadman@gmail.com>
2021-08-20 17:10:02 +0200

    Query marshal/unmarshal support

--- operator.go
+++ operator.go
@@ -2,6 +2,7 @@ package gojq
 
 import (
 	"encoding/json"
+	"fmt"
 	"maps"
 	"math"
 	"math/big"
@@ -38,6 +39,62 @@ const (
 	OpUpdateMod
 	OpUpdateAlt
 )
+
+// String implements [fmt.Stringer].
+func OperatorFromString(s string) Operator {
+	switch s {
+	case "|":
+		return OpPipe
+	case ",":
+		return OpComma
+	case "+":
+		return OpAdd
+	case "-":
+		return OpSub
+	case "*":
+		return OpMul
+	case "/":
+		return OpDiv
+	case "%":
+		return OpMod
+	case "==":
+		return OpEq
+	case "!=":
+		return OpNe
+	case ">":
+		return OpGt
+	case "<":
+		return OpLt
+	case ">=":
+		return OpGe
+	case "<=":
+		return OpLe
+	case "and":
+		return OpAnd
+	case "or":
+		return OpOr
+	case "//":
+		return OpAlt
+	case "=":
+		return OpAssign
+	case "|=":
+		return OpModify
+	case "+=":
+		return OpUpdateAdd
+	case "-=":
+		return OpUpdateSub
+	case "*=":
+		return OpUpdateMul
+	case "/=":
+		return OpUpdateDiv
+	case "%=":
+		return OpUpdateMod
+	case "//=":
+		return OpUpdateAlt
+	default:
+		return 0
+	}
+}
 
 // String implements [fmt.Stringer].
 func (op Operator) String() string {
@@ -91,7 +148,7 @@ func (op Operator) String() string {
 	case OpUpdateAlt:
 		return "//="
 	default:
-		panic(op)
+		return ""
 	}
 }
 
@@ -187,6 +244,26 @@ func (op Operator) getFunc() string {
 	default:
 		panic(op)
 	}
+}
+
+func (op Operator) MarshalJSON() ([]byte, error) {
+	if op == 0 {
+		return json.Marshal(nil)
+	}
+	return json.Marshal(op.String())
+}
+
+func (op *Operator) UnmarshalJSON(text []byte) error {
+	var s string
+	err := json.Unmarshal(text, &s)
+	if err != nil {
+		return err
+	}
+	*op = OperatorFromString(s)
+	if *op == 0 {
+		return fmt.Errorf("unknown operator %v", s)
+	}
+	return nil
 }
 
 func binopTypeSwitch(
